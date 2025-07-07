b0ab49f0fe0577adc955e30f8226216ad6fe930e
jemarc27 <delarosajemarc3@gmail.com>
2025-07-07 11:31:50 +0800

    worldtime

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:my_app/pages/Dashboard.dart';
 import 'package:my_app/pages/Listitem.dart';
 import 'package:my_app/pages/NewCoffee.dart';
-
+import 'package:my_app/World_Time/Home.dart';
+import 'package:my_app/World_Time/loading.dart';
+import 'package:my_app/World_Time/choose_location.dart';
 
 void main() {
   runApp(MaterialApp(
  routes: {
    '/' : (context) => Listitem(),
-   '/Add' : (context) => AddCoffee()
+   '/Add' : (context) => AddCoffee(),
+   '/dashboard' : (context) => Dashboard(),
+
+   // word time routes
+          '/Home' : (context) => Home(),
+   '/loading' : (context) => Loading(),
+   '/choose_location' : (context) => ChooseLocation()
+
+
+
  },
   ));
 }
